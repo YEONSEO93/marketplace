f0cf4750fd62f928cfd890a3d2fb848df2b799f3
Yeonseo Ko <yeooonseo@gmail.com>
2024-08-10 15:25:36 +1000

    Authentication-Github, Tab-bar

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -4,25 +4,32 @@ import FormButton from "@/components/button";
 import FormInput from "@/components/input";
 import SocialLogin from "@/components/social-login";
 import { useFormState } from "react-dom";
-import { handleForm } from "./actions";
-
+import { logIn } from "./actions";
+import { PASSWORD_MIN_LENGTH } from "@/lib/constants";
 
 export default function LogIn() {
-  const [state, action] = useFormState(handleForm, null);
+  const [state, dispatch] = useFormState(logIn, null);
   return (
     <div className="flex flex-col gap-10 py-8 px-6">
       <div className="flex flex-col items-center p-8 gap-10 *:font-medium">
         <h1 className="text-9xl">🥯 </h1>
-        <h2 className="text-4xl">Bagel Marketplace</h2>
+        <h1 className="text-4xl">Bagel Marketplace</h1>
       </div>
-
-      <form action={action} className="flex flex-col gap-3">
-        <FormInput name="email" type="email" placeholder="Email" required />
+      <form action={dispatch} className="flex flex-col gap-3">
+        <FormInput
+          name="email"
+          type="email"
+          placeholder="Email"
+          required
+          errors={state?.fieldErrors.email}
+        />
         <FormInput
           name="password"
           type="password"
           placeholder="Password"
           required
+          minLength={PASSWORD_MIN_LENGTH}
+          errors={state?.fieldErrors.password}
         />
         <FormButton text="Log in" />
       </form>
